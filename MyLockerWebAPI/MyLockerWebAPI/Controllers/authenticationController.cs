1ac70963959e6988b929b084eb2ca59849ff2896
shaheerghofran123 <39410043+shaheerghofran123@users.noreply.github.com>
2018-05-21 15:55:34 +0500

    Add files via upload

---
+++ MyLockerWebAPI/MyLockerWebAPI/Controllers/authenticationController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyLockerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class authenticationController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Shaheer";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public JsonResult Post([FromBody]UserCreds userCreds)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=DABEER-DEVELOPE; Initial Catalog=MyLocker; User Id=sa;password=admin;");
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM users WHERE password_hash=sys.fn_varbintohexstr(HASHBYTES('MD5','" + userCreds.password + "')) AND username='" + userCreds.username + "'", sqlConnection);
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCommand.CommandText, sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlConnection.Open();
+            sqlAdapter.Fill(dataTable);
+            UserResponseBody user = new UserResponseBody();
+            user.id = (Guid)dataTable.Rows[0]["id"];
+            var token = Guid.NewGuid();
+            user.token = token;
+            user.expiry = DateTime.Now.AddDays(7);
+            SqlCommand sqlCommand2 = new SqlCommand("INSERT INTO user_tokens(id,user_id,token,expiry,created,modified) VALUES('" +
+                Guid.NewGuid()+"','"+user.id+"','"+token+"','"+DateTime.Now.AddDays(7)+"','"+DateTime.Now+"','"+DateTime.Now+"')", sqlConnection);
+            sqlCommand2.ExecuteNonQuery();
+
+            //SqlCommand sqlCommand3 = new SqlCommand("UPDATE user SET last_login='"+DateTime.Now+"'", sqlConnection);
+            //sqlCommand3.ExecuteNonQuery();
+            sqlConnection.Close();
+            return new JsonResult(user);
+        }
+        public class UserCreds
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+        public class UserResponseBody
+        {
+            public Guid id { get; set; }
+            public Guid token { get; set; }
+            public DateTime expiry { get; set; }
+        }
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyLockerWebAPI/MyLockerWebAPI/Controllers/registrationController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyLockerWebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Register")]
+    public class registrationController : Controller
+    {
+        // GET: api/Register
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Register/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Register
+        [HttpPost]
+        public JsonResult Post([FromBody]UserToSave user)
+        {
+            string conn = "Server=tcp:mylockersqlserver.database.windows.net,1433;Initial Catalog=MyLocker;Persist Security Info=False;User ID=sqladmin;Password=Pass1984;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //string conn = "Data Source=DABEER-DEVELOPE; Initial Catalog=MyLocker; User Id=sa;password=admin;";
+
+            SqlConnection sqlConnection = new SqlConnection(conn);
+            Guid roleId = Guid.NewGuid();
+            Guid userId = Guid.NewGuid();
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO user_role(id,name,description,created,modified) VALUES('" +roleId+
+                user.roleName+ "','"+user.roleDescription+"','" + DateTime.Now + "','NULL')", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+
+            SqlCommand sqlCommand2 = new SqlCommand("INSERT INTO users(username,password_hash,dob,first_name,last_name," +
+                "email,status,reset_token,reset_expiry,last_login,profile_pic_url,NIN,is_social_login,created,modified,id," +
+                "role_id,guardian_id) VALUES('" + user.userName + "',sys.fn_varbintohexstr(HASHBYTES('MD5','" + user.password + "'))" +
+                ",'" + user.dob + "','" + user.firstName + "','" + user.lastName + "','" + user.emailId + "','active','NULL','NULL'" +
+                "'NULL','" + user.profilePicUrl + "','" + user.nationalInsNo + "','" + user.isSocialLogin + "','" + DateTime.Now + "','NULL','"
+                + userId + "','" + roleId + "','NULL')", sqlConnection);
+            sqlCommand2.ExecuteNonQuery();
+
+            SqlCommand sqlCommand3 = new SqlCommand("INSERT INTO user_details(user_id,country,state,city,zip_code,address_line_1," +
+                "address_line_2,employer_name,employer_status,education,marital_status,club_name,council_no,created,modified" +
+                "VALUES('" +userId + "','"+user.country+"','"+user.state+"','"+user.town+"','"+user.zipCode+"','"+user.addressLine1+"','"+
+                user.addressLine2 + "','" + user.employerName + "','" +user.employerStatus + "','" + user.education + "','"+user.MaritalStatus
+                +"','"+user.clubName+"','"+user.countcilNo+"','"+DateTime.Now+"','NULL')", sqlConnection);
+            sqlCommand3.ExecuteNonQuery();
+            sqlConnection.Close();
+
+
+            return new JsonResult(user);
+
+        }
+        public class UserToSave
+        {
+            public Guid id { get; set; }
+            public string roleName { get; set; }
+            public string MaritalStatus { get; set; }
+            public string userName { get; set; }
+            public string countcilNo { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string emailId { get; set; }
+            public string password { get; set; }
+            public string profilePicUrl { get; set; }
+            public DateTime dob { get; set; }
+            public string country { get; set; }
+            public string addressLine1 { get; set; }
+            public string addressLine2 { get; set; }
+            public string employerStatus { get; set; }
+            public string state { get; set; }
+            public string zipCode { get; set; }
+           
+            public string clubName { get; set; }
+            public string employerName { get; set; }
+            public string town { get; set; }
+            public bool isSocialLogin { get; set; }
+            public string nationalInsNo { get; set; }
+            public string roleDescription { get; set; }
+           
+            public string education { get; set; }
+            
+            public bool isUnder13 { get; set; }
+        }
+        // PUT: api/Register/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
